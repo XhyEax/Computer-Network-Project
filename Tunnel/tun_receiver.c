742fbd9ec5bcbcbdf5de2cd84d1ce98375979cc1
XhyEax <xhyeax@qq.com>
2021-05-20 19:28:51 +0800

    edit comment

--- Tunnel/tun_receiver.c
+++ Tunnel/tun_receiver.c
@@ -6,7 +6,7 @@ int main()
     char my_src_mac_address[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     char tun_dst_mac_address[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     bind_socket(my_src_mac_address, "127.0.0.1", 4321);
-    //打包后发给隧道路由
+    //解包
     tun_receiver();
     return 0;
 }
