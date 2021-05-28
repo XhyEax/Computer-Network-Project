74d64261fe7af9944914421273d05787c8561bee
XhyEax <xhyeax@qq.com>
2021-05-28 14:30:53 +0800

    rename var

--- Tunnel/packer.c
+++ Tunnel/packer.c
@@ -3,9 +3,9 @@
 int main()
 {
     char my_src_mac_address[6] = {0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa};
-    char tun_dst_mac_address[6] = {0xbb, 0xbb, 0xbb, 0xbb, 0xbb, 0xbb};
+    char my_dst_mac_address[6] = {0xbb, 0xbb, 0xbb, 0xbb, 0xbb, 0xbb};
     bind_socket("eth0", my_src_mac_address, "", 2333);
-    set_dstinfo(tun_dst_mac_address, "", 2333);
+    set_dstinfo(my_dst_mac_address, "", 2333);
     packer();
     return 0;
 }
